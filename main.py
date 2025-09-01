09ea94c44b82ac4ccb9e668dd61fd31c09e9b8f0
J <finleyj415@gmail.com>
2025-09-01 09:59:29 -0400

    Changes to main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-import auth
+import json, time
 usernames = ["Uno","Dos", "Tres", "Quattro", "Cinco"]
 
 def login() -> bool:
     print("Initializing...")
-    username = input(str("Please enter your username: "))
+    time.sleep(3)
+    username = input("Please enter your username: ")
     if username in usernames:
         print("Welcome back, " + username + "!")
         return True
     else:
         print("Username not recognized.")
         return False
-
-if login(True):
-    print("Access granted.")
-else:
-    print("Access denied.")
