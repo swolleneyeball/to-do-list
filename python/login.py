043386b0b39e24101cc8c21660d9c27dcd152cd1
J <finleyj415@gmail.com>
2025-09-02 10:34:28 -0400

    Modified login.py

--- python/login.py
+++ python/login.py
@@ -1,15 +1,16 @@
 import json, time
 
 with open('data/users.json') as users:
-    usernames = json.load(users)
-print(usernames)
-print(type(usernames[users]))
+    user_data = json.load(users)
+print(user_data)
+
+for user in user_data:
+    print(user['name'])
 
 def login(auth):
     print("Initializing...")
-    time.sleep(2)
     username = input("Please enter your username ->  ")
-    if username in usernames:
+    if username in user_data:
         print("Welcome back, " + username + "!")
         auth = True
         if auth == True:
@@ -25,13 +26,3 @@ def login(auth):
 
 auth_status = login(auth=bool)
 print(auth_status)
-
-def to_do_list():
-    if auth_status == True:
-        print("Accessing to-do list...")
-        time.sleep(2)
-        print("To-do list accessed.")
-    else:
-        print("Access denied.")
-
-to_do_list()
